ba93211506f0cf4a2917e2705f51d9d0a393b774
Daniel Kessler <sinani201@gmail.com>
2015-08-13 21:03:10 -0700

    server now on 0.0.0.0

--- sl_gameserver.py
+++ sl_gameserver.py
@@ -171,7 +171,7 @@ if __name__ == "__main__":
     factory.protocol = SLServerProtocol
 
     loop = asyncio.get_event_loop()
-    coro = loop.create_server(factory, "127.0.0.1", 443)
+    coro = loop.create_server(factory, "0.0.0.0", 443)
     server = loop.run_until_complete(coro)
 
     try:
